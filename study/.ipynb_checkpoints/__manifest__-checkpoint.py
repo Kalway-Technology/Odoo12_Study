e16f0118ec697d88ebb80b98630147da34821e01
james <james@Kalway.cn>
2018-12-07 11:55:31 +0000

    test

--- study/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ study/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -3,31 +3,33 @@
     'name': "study",
 
     'summary': """
-        Pscloud 培训""",
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        学生管理系统
+        Long description of module's purpose
     """,
 
-    'author': "Pscloud",
-    'website': "http://www.mypscloud.com",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'tools',
-    'version': '12.0.1
-   # any module necessary for this one to work correctly
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-#         'views/templates.xml',
+        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-#         'demo/demo.xml',
+        'demo/demo.xml',
     ],
 }
--- study/models/models.py
+++ study/models/models.py
@@ -2,14 +2,22 @@
 
 from odoo import models, fields, api
 
-# class study(models.Model):
-#     _name = 'study.study'
+class Study_Iterm(models.Model):
+    _name = 'study.iterm'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(u'名称', size=64, translate=True)
+    person_id = fields.Many2one('res.partner', u'负责人')
+    description = fields.Text(u'描述')
+    lession_id = fields.One2many('Study_Class',‘Iterm_id’,u'课程')
+    
+    
+class Study_Class(models.Model):
+    _name = 'study.class'
+
+    name = fields.Char(u'名称', size=64, translate=True)
+    teacher = fields.Many2one('res.partner', u'老师')
+    start_time = fields.Datetime(u'开始时间')
+    end_time = fields.Datetime(u'结束时间')
+    seat_number = fields.Integer(u'座位数')
+    study = fields.Many2many('res.partner','res_partner_study_rel','name',u'学生')
+    Iterm_id = fields.Many2one('Study_Iterm',u'课程')
